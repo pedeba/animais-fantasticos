eb8d5339db14701d4c84ae92a1b3a46a07c261b3
Pedro de Barros <pedropedeba@gmail.com>
2020-09-23 17:27:01 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initScrollSuave from  './modules/scroll-suave.js'
 import initScrollAnima from './modules/scroll-animacao.js'
-import initAccordion from './modules/accordion-list.js'
+import Accordion from './modules/accordion-list.js'
 import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
@@ -10,10 +10,11 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js'
 import initDataObject from './modules/data-object.js'
 import initFetchAnimais from './modules/fetch-animais.js'
 
-
+const accordion = new Accordion('.js-accordion dt')
+accordion.init()
 initScrollSuave()
 initScrollAnima()
-initAccordion()
+
 initTabNav()
 initModal()
 initTooltip()
