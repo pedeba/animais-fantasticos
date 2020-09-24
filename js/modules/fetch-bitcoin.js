08512c0da578f370de152c04d21beb022da6876c
Pedro de Barros <pedropedeba@gmail.com>
2020-09-24 17:34:22 -0300

    refatorado-fectchbitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,10 +1,10 @@
 
-export default function initFetchBitcoin(){
+export default function fetchBitcoin(url, target){
     
-        fetch('https://blockchain.info/ticker')
+        fetch(url)
         .then(response=>response.json())
         .then((json)=>{
-        const btcpreco = document.querySelector('.btc-preco')
+        const btcpreco = document.querySelector(target)
         btcpreco.innerText = (100/json.BRL['buy']).toFixed(4)
         })
         .catch((erro)=>{
